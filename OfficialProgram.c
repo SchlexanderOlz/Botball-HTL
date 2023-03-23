3e12de7038dd6787bc6749160eac7f514996584c
JJTV <63007476+JohnGrubba@users.noreply.github.com>
2023-03-23 11:41:05 +0100

    Nice Try but we will start over

--- OfficialProgram.c
+++ OfficialProgram.c
@@ -19,34 +19,48 @@ double map(double x, double in_min, double in_max, double out_min, double out_ma
 }
 
 void drive(int distance_cm, int speed, int angle){
+    printf("New Drive Instance");
     float right_speed = speed;
     float left_speed = speed;
     double mp = map(speed, 50, 100, 2, 1);
-    float direction = get_compass_angle();
+    float direction = fabs(get_compass_angle() * (180 / 3.1415));
+    printf("%f", direction);
     int start_seconds = (int)time(NULL);
-    while(start_seconds + (62.5 * distance_cm * mp /1000)  > (int)time(NULL)){
-        if (get_compass_angle() > direction){
+    const float mult = 0.1;
+    float new_dir = direction;
+    while(start_seconds + (62.5 * distance_cm * mp / 1000)  > (int)time(NULL)){
+        new_dir = fabs(get_compass_angle() * (180 / 3.1415));
+        printf("%f\n", new_dir);
+        if (new_dir < direction){
             // Correct angle
-            right_speed *= 1.1;
+           	left_speed -= mult;
+            right_speed += mult;
+        }
+        else if (new_dir > direction){
+            right_speed -= mult;
+            left_speed += mult;
         }
         else{
-            left_speed *= 1.1;
+            right_speed = speed;
+            left_speed = speed;
         }
         motor(right_wheel, right_speed);
         motor(left_wheel, left_speed);
+        msleep(100);
     }
     printf("%f", mp);
-    //msleep(62.5 * distance_cm * mp);
     motor(right_wheel, 0);
     motor(left_wheel, 0);
 }
+
 void turnDegrees(int degrees, int speed){
-    double degs_start = fabs(get_compass_angle() * (180 / 3.1415));
-    printf("%f\n", degs_start);
-    motor(right_wheel, 10);
-    motor(left_wheel, -10);
-    while(degs_start + degrees > fabs(get_compass_angle() * (180 / 3.1415))){
-        printf("%f\n", fabs(get_compass_angle() * (180 / 3.1415)));
+    int degs_start = (int)fabs(get_compass_angle() * (180 / 3.1415)) + degrees;
+    printf("\n%d\n", degs_start);
+    motor(right_wheel, speed);
+    motor(left_wheel, -speed);
+    while(degs_start >= (int)fabs(get_compass_angle() * (180 / 3.1415))){
+        printf("\n%d\n", (int)fabs(get_compass_angle() * (180 / 3.1415)));
+        msleep(10);
         continue;
     };
     motor(right_wheel, 0);
@@ -69,9 +83,11 @@ int main()
     motor(right_wheel, 20);
     motor(left_wheel, -20);
     calibrate_compass();
-    drive(5, 50, 0);
-    drive(5, 50, 90);
-    drive(10, 50, 180);
+    //set_compass_params(-7.364000, -14.885000, -36.187000, 0.491131, -0.057895, 1.114096, 1.242885);
+    
+    drive(15, 20, 0);
+    turnDegrees(90, 10);
+    drive(15, 20, 90);
     return 0;
 }
 
